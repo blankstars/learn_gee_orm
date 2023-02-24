92a7b788c41e85029ce286bef6264bd82af25600
blankstars <blankstars@gmail.com>
2023-02-24 17:56:57 +0800

    add

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/blankstars/gee_orm
+
+go 1.19
+
+require github.com/mattn/go-sqlite3 v1.14.16
---
+++ session/raw.go
@@ -0,0 +1,56 @@
+package session
+
+import (
+	"database/sql"
+	"github.com/blankstars/gee_orm/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVals []any
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...any) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVals = append(s.sqlVals, values...)
+	return s
+}
+
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVals)
+	if result, err = s.db.Exec(s.sql.String(), s.sqlVals...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVals)
+	return s.db.QueryRow(s.sql.String(), s.sqlVals...)
+}
+
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVals)
+	if rows, err = s.db.Query(s.sql.String(), s.sqlVals...); err != nil {
+		log.Error(err)
+	}
+	return
+}
