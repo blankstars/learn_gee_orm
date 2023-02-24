d1dcd49ab7290dce16ce538574e9fa44c09fa11f
blankstars <blankstars@gmail.com>
2023-02-24 19:48:00 +0800

    add

---
+++ schema/schema.go
@@ -0,0 +1,25 @@
+package schema
+
+import "github.com/blankstars/gee_orm/dialect"
+
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+type Schema struct {
+	Model      interface{}
+	Name       string
+	Fields     []*Field
+	FieldNames []string
+	fieldMap   map[string]*Field
+}
+
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+func Parse(desc any, d dialect.Dialect) *Schema {
+
+}
